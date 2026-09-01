9e2d7e0434684a4693b296888f492ddd42be3581
FlavioKawan <kawanflavio66@gmail.com>
2026-09-01 00:13:33 -0300

    fix: trata falha ao visitar URL

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from crawler import queue_control
 
+<<<<<<< HEAD
 url = "https://rdcviagens.com.br/"
+=======
+url = "https://tecnocomp.com.br"
+>>>>>>> 34c2513 (fix: trata falha ao visitar URL)
 queue_control(url)
